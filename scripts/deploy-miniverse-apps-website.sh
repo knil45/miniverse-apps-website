5a97e6188cdc30512b665a3bff4a8ca62b927f46
knil45 <jordisalvans5@msn.com>
2025-05-05 10:48:58 +0100

    Fix names

--- scripts/deploy-miniverse-apps-website.sh
+++ scripts/deploy-miniverse-apps-website.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# Copy this file manually to VPS folder /root/apps
 
-APP_DIR=~/apps/miniverse-website
+APP_DIR=~/apps/miniverse-apps-website
 REPO_URL=git@github.com-miniverse-apps-website:knil45/miniverse-apps-website.git
 BRANCH=master
 
@@ -13,4 +14,4 @@ fi
 
 cd $APP_DIR
 npm install
-pm2 restart miniverse-website || pm2 start src/index.js --name miniverse-website
+pm2 restart miniverse-apps-website || pm2 start src/index.js --name miniverse-apps-website

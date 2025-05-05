9925e8142c1be68fa84bb42bda92132e3b4562dd
knil45 <jordisalvans5@msn.com>
2025-05-05 14:38:15 +0100

    Fix shell

--- scripts/vps-pull-miniverse-apps-website.sh
+++ scripts/vps-pull-miniverse-apps-website.sh
@@ -5,11 +5,12 @@ APP_DIR=/var/www/miniverse-apps-website
 REPO_URL=git@github.com-miniverse-apps-website:knil45/miniverse-apps-website.git
 BRANCH=main
 
+cd $APP_DIR
+
 # Pull changes
 git pull origin $BRANCH
 
 # Refresh npm
-cd $APP_DIR
 npm install
 
 # Restart server
